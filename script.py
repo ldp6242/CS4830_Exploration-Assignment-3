d4c0a9714d2f826de34485415c14fa87e3e3d5ad
ldp6242 <ldp535@mail.missouri.edu>
2014-10-09 20:20:44 -0500

    Create script.py

---
+++ script.py
@@ -0,0 +1,31 @@
+#PygLatin
+
+pyg = 'ay'
+
+original = raw_input('Enter a word:')
+
+if len(original) > 0 and original.isalpha():
+    print original
+    word = original.lower()
+    first =  word[0]
+    new_word = word[1:] + first + pyg
+else:
+    print 'empty'
+    
+#BillFunction
+
+def tax(bill):
+    """Adds 8% tax to a restaurant bill."""
+    bill *= 1.08
+    print "With tax: %f" % bill
+    return bill
+
+def tip(bill):
+    """Adds 15% tip to a restaurant bill."""
+    bill *= 1.15
+    print "With tip: %f" % bill
+    return bill
+    
+meal_cost = 100
+meal_with_tax = tax(meal_cost)
+meal_with_tip = tip(meal_with_tax)
